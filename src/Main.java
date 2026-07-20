15d61f3f5ea452da9f4f85f85df4a091a7321d30
rafa <abarca1991martinez@gmail.com>
2026-07-20 11:22:30 +0200

    Añadimos ejercicio de verificacion de contraseña

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,35 @@
-public class Main {
 
+import java.util.Scanner;
+
+public class Main {
+    
+
+     
     public static void main(String[] args) {
 
-        System.out.println("Hola Rafa");
-        System.out.println("Estoy aprendiendo Java");
-        System.out.println("Mi objetivo es ser programador");
+        String contrasenia = "Java";
+         int intentos = 10;
         
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escriba la contraseña ");
+        String contrasenia_ingresada = sc.nextLine();
+
+        while(!contrasenia.equals(contrasenia_ingresada) && intentos > 0){
+            
+            
+                System.out.println("Esccriba de nuevo la contreseña ");
+                contrasenia_ingresada = sc.nextLine();
+                intentos--;
+                
+        
+
+        }
+
+        String mensaje =(contrasenia.equals(contrasenia_ingresada))?
+         "Su contrasena es correcta" : "Usuario bloqueadol"; 
+            
+       
+         System.out.println(mensaje);
+   
     }
-}
+ }
