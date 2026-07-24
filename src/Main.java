e64f1da8a1b1c8d9138bad082787ef85d493a91a
rafa <abarca1991martinez@gmail.com>
2026-07-24 10:25:48 +0200

    Programa de autentificación con tablas de multiplicar

--- src/Main.java
+++ src/Main.java
@@ -7,29 +7,75 @@ public class Main {
      
     public static void main(String[] args) {
 
-        String contrasenia = "Java";
-         int intentos = 10;
-        
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Escriba la contraseña ");
-        String contrasenia_ingresada = sc.nextLine();
+         Scanner sc = new Scanner(System.in);
 
-        while(!contrasenia.equals(contrasenia_ingresada) && intentos > 0){
+         String contrasenia = "Java123";
+         
+         int nuevoNuemero;
+         String pregunta = "s";
+         int numIntentos = 3;
+         String insertacontr;
+         do{
             
+
+
+            System.out.println("Introduce la contraseña ");
+            insertacontr = sc.nextLine();
             
-                System.out.println("Esccriba de nuevo la contreseña ");
-                contrasenia_ingresada = sc.nextLine();
-                intentos--;
+           if(contrasenia.equals(insertacontr)){
+
+            System.out.println("contraseña correcta ");
+
+       } else {
+
+              numIntentos--;
                 
-        
-
-        }
-
-        String mensaje =(contrasenia.equals(contrasenia_ingresada))?
-         "Su contrasena es correcta" : "Usuario bloqueadol"; 
+                System.out.println("Tienes " + numIntentos + " intentos.");
+          
             
-       
-         System.out.println(mensaje);
-   
-    }
- }
+           }
+            
+         } while (!contrasenia.equals(insertacontr) && numIntentos >= 1 );
+
+            
+                if(contrasenia.equals(insertacontr)){
+
+                    System.out.println("Bienvenido. ");
+
+                    do {
+                        System.out.println("que numero quieres multipicar? ");
+                        nuevoNuemero = sc.nextInt();
+                        for(int i = 1; i <= 10; i++){
+                            System.out.println(
+                            nuevoNuemero + " * " + i + " = " + (nuevoNuemero * i));
+                        }
+
+                        System.out.println("Quieres volver a imprimir otra tabla? s/n ");
+                        sc.nextLine();
+                        pregunta = sc.nextLine();
+
+                        
+                    } while (pregunta.equals("s"));
+                    
+                    System.out.println("Hasta pronto ");
+                
+                
+                
+                } else {
+
+                     System.out.println("Usuario bloqueado. ");
+                }
+                    
+            
+                 
+            
+               
+            }
+
+             
+            }
+           
+
+        
+    
+
