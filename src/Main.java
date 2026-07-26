e8e7789cb098bc1b4ad164f6e90287794a38cca3
Rafa Martinez <abarca1991martinez@gmail.com>
2026-07-26 21:44:38 +0200

    Ejercicios de arrays

--- src/Main.java
+++ src/Main.java
@@ -7,40 +7,83 @@ public class Main {
      
     public static void main(String[] args) {
         
- Scanner sc = new Scanner(System.in);
+Scanner sc = new Scanner(System.in);
 
-int[] numeros = new int[5];
+int[] numeros = new int[10];
+
 for(int i = 0; i < numeros.length; i++){
-System.out.println("Que numero quieres? ");
-
-numeros[i] = sc.nextInt();
+    System.out.println("Que numeros quieres añadir? ");
+    numeros[i] = sc.nextInt();
 }
 
-int num;
-boolean encontrado = false;
-
-System.out.println("Que numero te gustaria buscar? ");
-num = sc.nextInt();
-
-for ( int i = 0; i < numeros.length; i++){
+// Ejecicio mayo y menor
+int numMayor = numeros[0];
+int numMenor = numeros[0];
 
 
+for(int i = 0; i < numeros.length; i++){
 
-
-if( numeros[i] == num){
-
-    encontrado = true;
-
-} 
-
+    if(numeros[i] > numMayor){
+        numMayor = numeros[i];
+    } else if(numeros[i] < numMenor){
+        numMenor = numeros[i];
+    }
 }
-    if(encontrado){
-        System.out.println("Existe ");
-    }else {
-        System.out.println("No existe ");
+    System.out.println("El numero mayor es: " + numMayor);
+    System.out.println("El numero menor es: " + numMenor);
+    
+// Suma y media
+
+int suma = 0;
+double media;
+
+for(int i = 0; i < numeros.length; i++){
+
+    suma += numeros[i];
+    
+}
+System.out.println("La suma de todos los valores es: " + suma);
+
+media = suma / numeros.length;
+
+System.out.println("la media del array es: " + media );
+
+int numPares = 0;
+int numImpares = 0;
+for(int i = 0; i < numeros.length; i++){
+     if (numeros[i] % 2 == 0) {
+        numPares++;
+     } else {
+        numImpares++;
+     }
+    
+}
+
+    System.out.println("Hay " + numPares + " pares");
+    System.out.println("Hay " + numImpares + " imapres");
+
+
+    // Buscar un numero
+    boolean existe = false;
+    int buscarNum;
+    System.out.println("Que numero quieres buscar? ");
+
+    buscarNum = sc.nextInt();
+
+    for(int i = 0; i < numeros.length; i++){
+        if (numeros[i] == buscarNum){
+            existe = true;
+        }
     }
 
-     sc.close();
+    if (existe) {
+        System.out.println("El numero que buscas si esta dentro del array. ");
+    } else {
+        System.out.println("El numero que buscas no esta dentro del array. ");
+    }
+    
+
+ sc.close();
 }
 
 }
